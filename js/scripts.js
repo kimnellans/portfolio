c1459149240c2c695f381b138e1ee9aedc49cd48
kimnellans <kim.nellans@gmail.com>
2023-09-25 15:30:18 -0600

    test new code for animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 console.log("Hello World");
 
 
+
+// Add the new script below
+
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.querySelector('.case-study-link');
+    const emojis = document.querySelectorAll('.emoji');
+  
+    link.addEventListener('mouseenter', function() {
+      emojis.forEach((emoji, index) => {
+        setTimeout(() => {
+          emoji.style.opacity = '1';
+          emoji.style.transform = 'scale(1)';
+        }, index * 50); // 50ms delay between each emoji animation for a rapid explosion
+      });
+    });
+  
+    link.addEventListener('mouseleave', function() {
+      emojis.forEach((emoji) => {
+        emoji.style.opacity = '0';
+        emoji.style.transform = 'scale(0)';
+      });
+    });
+  });
+  
