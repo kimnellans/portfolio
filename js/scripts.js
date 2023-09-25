b759de6a46e4a11677751e31ff9ce50f6ea0f67a
kimnellans <kim.nellans@gmail.com>
2023-09-25 17:04:52 -0600

    updates for effect

--- js/scripts.js
+++ js/scripts.js
@@ -44,26 +44,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-  //js for YUJ interaction
-  function bubbles() {
-    $.each($(".yuj-link"), function(){
-       var bubblecount = ($(this).width()/50)*10;
-       var emojis = ["🧘", "🕉️", "✨", "🌙", "☮️", "🦄", "🌈", "💎", "❤️", "☀️", "🌒", "🧚‍♀️", "🙏", "📿"];
-       for(var i = 0; i <= bubblecount; i++) {
-          var size = ($.rnd(40,80)/10);
+ //js for YUJ interaction
+function bubbles() {
+  $.each($(".yuj-link"), function(){
+      var bubblecount = ($(this).width()/50)*10;
+      var emojis = ["🧘", "🕉️", "✨", "🌙", "☮️", "🦄", "🌈", "💎", "❤️", "☀️", "🌒", "🧚‍♀️", "🙏", "📿"];
+      for(var i = 0; i <= bubblecount; i++) {
+          var size = ($.rnd(20,40)/10); // Adjusted size for better visibility
           var randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-          $(this).append('<span class="yuj-emoji" aria-hidden="true" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;font-size:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;">' + randomEmoji + '</span>');
-       }
-    });
- }
- 
- jQuery.rnd = function(m,n) {
-       m = parseInt(m);
-       n = parseInt(n);
-       return Math.floor( Math.random() * (n - m + 1) ) + m;
- }
- 
- $(document).ready(function() {
-     bubbles();
- });
- 
+          $(this).append('<span class="yuj-emoji" aria-hidden="true" style="top:' + $.rnd(-10,110) + '%; left:' + $.rnd(-10,110) + '%;font-size:' + size + 'em;animation-delay: ' + ($.rnd(0,30)/10) + 's;">' + randomEmoji + '</span>');
+      }
+  });
+}
+
+jQuery.rnd = function(m,n) {
+  m = parseInt(m);
+  n = parseInt(n);
+  return Math.floor( Math.random() * (n - m + 1) ) + m;
+}
+
+$(document).ready(function() {
+  bubbles();
+});
