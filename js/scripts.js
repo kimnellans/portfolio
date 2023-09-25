3589a3839945120874988858c161600429bbb74d
kimnellans <kim.nellans@gmail.com>
2023-09-25 16:06:06 -0600

    upate js mouse leave event

--- js/scripts.js
+++ js/scripts.js
@@ -19,16 +19,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const y = e.clientY - rect.top;
 
     emojis.forEach((emoji) => {
-      const dx = (emoji.getAttribute('data-dx') || 0) * (x / rect.width - 0.5) * 10; // Adjusted multiplier for more dynamic movement
-      const dy = (emoji.getAttribute('data-dy') || 0) * (y / rect.height - 0.5) * 10; // Adjusted multiplier for more dynamic movement
+      const dx = (emoji.getAttribute('data-dx') || 0) * (x / rect.width - 0.5);
+      const dy = (emoji.getAttribute('data-dy') || 0) * (y / rect.height - 0.5);
       emoji.style.transform = `translate(${dx}px, ${dy}px)`;
     });
   });
 
   link.addEventListener('mouseleave', function() {
     emojis.forEach((emoji) => {
+      // Reset the styles immediately without any transition
+      emoji.style.transition = 'none'; 
       emoji.style.opacity = '0';
       emoji.style.animation = 'none';
+      emoji.style.transform = 'none';
     });
+    // Restore the transition after a brief timeout
+    setTimeout(() => {
+      emojis.forEach((emoji) => {
+        emoji.style.transition = 'opacity 0.3s ease-out';
+      });
+    }, 10);
   });
 });
