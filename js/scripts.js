7e9c423de34596e693ee73a49c4dc5dda00210cb
kimnellans <kim.nellans@gmail.com>
2023-09-25 15:45:38 -0600

    update fruits

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,20 @@ console.log("Hello World");
 // Add the new script below
 
 document.addEventListener('DOMContentLoaded', function() {
-    const link = document.querySelector('.case-study-link');
-    const emojis = document.querySelectorAll('.emoji');
-  
-    link.addEventListener('mouseenter', function() {
-      emojis.forEach((emoji) => {
-        emoji.style.opacity = '1';
-        emoji.style.transform = 'scale(1.2)'; // expand the emojis slightly for the "breathe" effect
-      });
-    });
-  
-    link.addEventListener('mouseleave', function() {
-      emojis.forEach((emoji) => {
-        emoji.style.opacity = '0';
-        emoji.style.transform = 'scale(0)';
-      });
+  const link = document.querySelector('.case-study-link');
+  const emojis = document.querySelectorAll('.emoji');
+
+  link.addEventListener('mouseenter', function() {
+    emojis.forEach((emoji) => {
+      emoji.style.opacity = '1';
+      emoji.style.animation = 'explode 0.5s forwards';
     });
   });
-  
+
+  link.addEventListener('mouseleave', function() {
+    emojis.forEach((emoji) => {
+      emoji.style.opacity = '0';
+      emoji.style.animation = 'none';
+    });
+  });
+});
